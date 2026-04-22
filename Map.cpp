dffb9cc230dcd8ad16c38a8d8f61dea2c0b3a979
PhucLai2110 <laipham2110@gmail.com>
2026-04-22 08:42:49 +0700

    Thêm file map.h và map.cpp

---
+++ Map.cpp
@@ -0,0 +1,50 @@
+﻿#include "Map.h"
+#include <iostream>
+
+cmap::cmap() {
+    // Constructor không cần làm gì nếu chỉ dùng ảnh nền
+}
+
+bool cmap::LoadTexturemap(int mapIndex, const std::string& filename) {
+    std::string folder = "img/map" + std::to_string(mapIndex) + "/";
+    std::string path = folder + filename;
+
+    if (!backgroundTexture.loadFromFile(path)) {
+        std::cerr << "Không thể load background map từ: " << path << std::endl;
+        return false;
+    }
+
+    backgroundSprite.setTexture(backgroundTexture);
+    useBackground = true;
+    return true;
+}
+
+void cmap::draw(sf::RenderWindow& window) {
+    if (useBackground) {
+        window.draw(backgroundSprite);
+    }
+}
+
+void cmap::setTowerPlacements(const std::vector<sf::Vector2f>& positions) {
+    towerPlacements = positions;
+    isPlaced.resize(positions.size(), false);
+}
+
+void cmap::resetTowers() {
+    std::fill(isPlaced.begin(), isPlaced.end(), false);
+}
+
+const std::vector<sf::Vector2f>& cmap::getTowerPlacements() const {
+    return towerPlacements;
+}
+
+bool cmap::isOccupied(int index) const {
+    if (index >= 0 && index < isPlaced.size())
+        return isPlaced[index];
+    return true;
+}
+
+void cmap::placeTower(int index) {
+    if (index >= 0 && index < isPlaced.size())
+        isPlaced[index] = true;
+}
---
+++ Map.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <cstring>
+#include <fstream>  
+#include <sstream>  
+
+class cmap {
+private:
+    sf::Texture backgroundTexture;
+    sf::Sprite backgroundSprite;
+    bool useBackground = false;
+    std::vector<sf::Vector2f> towerPlacements;  // Danh sách ô có thể đặt tower
+    std::vector<bool> isPlaced;                 // Đánh dấu ô nào đã đặt tower
+public:
+    cmap();
+
+    bool LoadTexturemap(int mapIndex, const std::string& filename); // Load ảnh nền
+    void draw(sf::RenderWindow& window); // Vẽ ảnh nền
+
+    void setTowerPlacements(const std::vector<sf::Vector2f>& positions);
+    void resetTowers();
+    const std::vector<sf::Vector2f>& getTowerPlacements() const;
+    bool isOccupied(int index) const;
+    void placeTower(int index);
+
+};
